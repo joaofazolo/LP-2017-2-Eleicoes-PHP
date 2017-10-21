c7570cac9734bedd6338d2242c6bc5f4c945131f
joaofazolo <jvfazolo@gmail.com>
2017-10-21 09:41:18 -0300

    Relatorio de votos nominais pronto

--- io/leitor.php
+++ io/leitor.php
@@ -13,6 +13,7 @@ if(isset($_POST['ArquivoEntrada']))
 
 if(isset($vetCandidatos)){
     NumeroDeVagas($vetCandidatos);
+    echo "<br>";
     TotalVotosNominais($vetCandidatos);
 
 }
@@ -29,10 +30,10 @@ function LerCSV($nome){
             if($linha[0][0]=="*")
                 $auxEleito = true;
             if(sizeof($split)==2)
-                $vetCandidatos[$i] = new Candidato($linha[1],$linha[2],new Partido($split[0],$split[1]),$auxEleito,$linha[4]);
+                $vetCandidatos[$i] = new Candidato($linha[1],$linha[2],new Partido($split[0],$split[1]),$auxEleito,str_replace('.',"",$linha[4]));
             else
-                $vetCandidatos[$i] = new Candidato($linha[1],$linha[2],new Partido($split[0],"nenhuma"),$auxEleito,$linha[4]);
-            $vetCandidatos[$i]->printCandidato();
+                $vetCandidatos[$i] = new Candidato($linha[1],$linha[2],new Partido($split[0],"nenhuma"),$auxEleito,str_replace('.',"",$linha[4]));
+            //$vetCandidatos[$i]->printCandidato();
             $i++;        
         }
     }
